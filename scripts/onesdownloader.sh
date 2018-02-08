7d2bed0b9b9868b32962eb38060887cf10d8bfc7
s.skurskiy@fundament.ua <s.skurskiy@fundament.ua>
2018-02-08 20:50:21 +0200

    Исправленная версия для загрузки

--- scripts/onesdownloader.sh
+++ scripts/onesdownloader.sh
@@ -1,30 +1,30 @@
+# this file from https://github.com/Infactum/onec_dock/blob/master/download.sh
+# see license notice for legal purpose
+
 #!/bin/bash
 #set -x
+source ./.env
 
-if [ -z "$V8USER" ]
+if [ -z "$USERNAME" ]
 then
     echo "USERNAME not set"
     exit 1
 fi
 
-if [ -z "$V8PASSW" ]
+if [ -z "$PASSWORD" ]
 then
     echo "PASSWORD not set"
     exit 1
 fi
 
-V8VERSION="$1"
-DOWNLOADIR="$2"
-
-if [ -z "$V8VERSION" ]
+if [ -z "$VERSION" ]
 then
     echo "VERSION not set"
     exit 1
 fi
 
 SRC=$(curl -c /tmp/cookies.txt -s -L https://releases.1c.ru)
-ACTION=$(echo "$SRC" | grep -oP '(?<=form id="loginForm" action=")[^"]+(?=")') 
-LT=$(echo "$SRC" | grep -oP '(?<=input type="hidden" name="lt" value=")[^"]+(?=")')
+ACTION=$(echo "$SRC" | grep -oP '(?<=form method="post" id="loginForm" action=")[^"]+(?=")')
 EXECUTION=$(echo "$SRC" | grep -oP '(?<=input type="hidden" name="execution" value=")[^"]+(?=")')
 
 curl -s -L \
@@ -32,31 +32,44 @@ curl -s -L \
     -b /tmp/cookies.txt \
     -c /tmp/cookies.txt \
     --data-urlencode "inviteCode=" \
-    --data-urlencode "lt=$LT" \
     --data-urlencode "execution=$EXECUTION" \
     --data-urlencode "_eventId=submit" \
-    --data-urlencode "username=$V8USER" \
-    --data-urlencode "password=$V8PASSW" \
+    --data-urlencode "username=$USERNAME" \
+    --data-urlencode "password=$PASSWORD" \
     https://login.1c.ru"$ACTION"
 
-if ! grep -q "onec_security" /tmp/cookies.txt
+if ! grep -q "TGC" /tmp/cookies.txt
 then
     echo "Auth failed"
     exit 1
 fi
 
-texttodownload="Скачать дистрибутив"
+mkdir -p $DOWNLOADIR
 
 CLIENTLINK=$(curl -s -G \
     -b /tmp/cookies.txt \
     --data-urlencode "nick=Platform83" \
-    --data-urlencode "ver=$V8VERSION" \
-    --data-urlencode "path=Platform\\${V8VERSION//./_}\\Platform\8_3_11_3034\windows.rar" \
+    --data-urlencode "ver=$VERSION" \
+    --data-urlencode "path=Platform\\${VERSION//./_}\\client.deb32.tar.gz" \
     https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')
-echo $CLIENTLINK
 
-mkdir -p $DOWNLOADIR
+SERVERINK=$(curl -s -G \
+    -b /tmp/cookies.txt \
+    --data-urlencode "nick=Platform83" \
+    --data-urlencode "ver=$VERSION" \
+    --data-urlencode "path=Platform\\${VERSION//./_}\\deb.tar.gz" \
+    https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')    
 
-wget --continue --load-cookies /tmp/cookies.txt -O $DOWNLOADIR/windows.rar "$SERVERINK"
+
+PLATGFORM32LINK=$(curl -s -G \
+    -b /tmp/cookies.txt \
+    --data-urlencode "nick=Platform83" \
+    --data-urlencode "ver=$VERSION" \
+    --data-urlencode "path=Platform\\${VERSION//./_}\\windows.rar" \           
+    https://releases.1c.ru/version_file | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив)')
+
+curl --fail -b /tmp/cookies.txt -o $DOWNLOADIR/client32.tar.gz -L "$CLIENTLINK"
+curl --fail -b /tmp/cookies.txt -o $DOWNLOADIR/server32.tar.gz -L "$SERVERINK"
+curl --fail -b /tmp/cookies.txt -o $DOWNLOADIR/server32.tar.gz -L "$PLATGFORM32LINK"
 
 rm /tmp/cookies.txt
